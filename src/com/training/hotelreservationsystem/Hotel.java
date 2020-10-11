1a74e4a5cec31a9895a4b16c9a2272e94a1b7adb
utkarshmega <utkarsheduc@gmail.com>
2020-10-11 18:42:06 +0530

    New Hotel added

---
+++ src/com/training/hotelreservationsystem/Hotel.java
@@ -0,0 +1,13 @@
+package com.training.hotelreservationsystem;
+
+public class Hotel {
+
+	private String hotelName;
+	private int rate;
+
+	Hotel(String hotelName, int rate) {
+		this.hotelName = hotelName;
+		this.rate = rate;
+	}
+
+}
--- src/com/training/hotelreservationsystem/HotelReservation.java
+++ src/com/training/hotelreservationsystem/HotelReservation.java
@@ -1,10 +1,30 @@
 package com.training.hotelreservationsystem;
 
+import java.util.*;
+
 public class HotelReservation {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to hotel reservation system");
+		Scanner sc = new Scanner(System.in);
+
+		ArrayList<Hotel> hotel = new ArrayList<>();
+
+		while (true) {
+
+			System.out.println("Enter hotel name");
+			String hotel_name = sc.next();
+			System.out.println("Enter rate for regular customer");
+			int rate = sc.nextInt();
+
+			hotel.add(new Hotel(hotel_name, rate));
+			System.out.println("To add more hotel Y/N");
+			char choice = sc.next().charAt(0);
+			if (choice == 'Y' || choice == 'y')
+				continue;
+			else
+				break;
+		}
 
 	}
 
