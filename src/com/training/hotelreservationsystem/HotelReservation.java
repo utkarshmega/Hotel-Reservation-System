b563866bb550a5eede8941914b910c7130a6cf32
utkarshmega <utkarsheduc@gmail.com>
2020-10-11 18:39:01 +0530

    Welcome to hotel reservation system

---
+++ src/com/training/hotelreservationsystem/HotelReservation.java
@@ -0,0 +1,11 @@
+package com.training.hotelreservationsystem;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to hotel reservation system");
+
+	}
+
+}
